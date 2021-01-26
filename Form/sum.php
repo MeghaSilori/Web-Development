e94d28d4224e5334ae72f214a32d8f6613ddd9ff
MeghaSilori <meghasilori7@gmail.com>
2021-01-26 20:02:26 +0530

    php

--- Form/sum.php
+++ Form/sum.php
@@ -1,13 +1,18 @@
 <?php 
-            $sum = $_POST['var1'] + $_POST['var2'];
-            echo "sum of two variable is $sum ";
+    if(isset($_POST['submit'])){
+        $sum = $_POST['var1'] + $_POST['var2'];
+        echo "sum of two variable is $sum ";
+    }
+    else{
+        echo "No input";
+    }
 ?>
 <html>
     <head>
         <title>PHP First Program</title>
     </head>
     <body>
-        <form method="POST" action="index.php">
+        <form method="POST" action="sum.php">
             Variable1 <input type="text" name="var1" >
             Variable2 <input type="text" name="var2" >
             <button class="submit" type="submit" value="submit">SUBMIT</button></td></tr>   	
