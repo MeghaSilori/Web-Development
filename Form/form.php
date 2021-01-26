33ef803bf53528bc1374ab0413a919074ec752b3
MeghaSilori <meghasilori7@gmail.com>
2021-01-26 20:43:40 +0530

    php

--- Form/form.php
+++ Form/form.php
@@ -3,11 +3,11 @@
         <title>Form Data on Table</title>
     </head>
     <body>
-        <form method="POST" action="index.php">
-            Name<input type="text" name="name" placeholder="enter your name"/><br>
-            E-mail<input type="email" name="mail" placeholder="enter your email"/><br>
-            Contact No.<input type="contact" name="contact" /><br>
-            City<Select name="city">
+        <form method="POST" action="form.php">
+            Name*<input type="text" name="name" placeholder="First Name" required/><br>
+            E-mail*<input type="email" name="mail" placeholder="Mail@example.com"required/><br>
+            Contact No.*<input type="contact" name="contact" placeholder="Phone Number" required/><br>
+            City*<Select name="city" required>
                 <option value="1"> Dehradun</option>
                 <option value="2"> Delhi</option>
                 <option value="3"> Chandigarh</option>
@@ -15,8 +15,8 @@
                 <option value="5"> Kolkata</option>
                 <option value="6"> Other</option>
                 </Select><br>
-            Course<input type="text" name="course"/><br>
-            Intrest<input type="radio"  name="I1"/>Sport
+            Course*<input type="text" name="course" required/><br>
+            Interest<input type="radio"  name="I1"/>Sport
                 <input type="radio"  name="I2"/>Program
                 <input type="radio" name="I3"/>Reading
                 <input type="radio" name="I4"/>Dance
@@ -31,7 +31,27 @@
             $ct= $_POST['contact'];
             $cty= $_POST['city'];
             $crse= $_POST['course'];
-            echo "$nm $ml";
+            echo" ";
+            echo "<table border='1px'>
+            <thead>
+            <tr>
+                <td>Name</td>
+                <td>Email ID</td>
+                <td>Contant No.</td>
+                <td>City</td>
+                <td>Course</td>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>$nm</td>
+                <td>$ml</td>
+                <td>$ct</td>
+                <td>$cty</td>
+                <td>$crse</td>
+            </tr>
+            </tbody>
+        </table> ";
         }
         else{
             echo "No input";
