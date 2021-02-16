d4c62ebefaffd137ec66f3cb8655353c6bfa8d12
MeghaSilori <meghasilori7@gmail.com>
2021-02-16 14:17:40 +0530

    Assignment3

--- Assignment3/config.php
+++ Assignment3/config.php
@@ -7,7 +7,7 @@ $databasename = "web";
 $conn = mysqli_connect($hostname,$username,$password,$databasename);
 if($conn){
     echo "Connection success...";
-}
+} 
 else{
     echo "Connection fail..." + mysqli_connect_error();
 }
